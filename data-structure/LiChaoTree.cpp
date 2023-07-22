76665ef050aa09a9e47f009b4c3b51d87521e385
b08901135@ntu.edu.tw <b08901135@ntu.edu.tw>
2023-07-22 12:52:39 +0900

    lichao tree done

--- data-structure/LiChaoTree.cpp
+++ data-structure/LiChaoTree.cpp
@@ -1,40 +1,61 @@
-namespace lichao {
-  struct line {
-    long long a, b;
-    line(): a(0), b(0) {}
-    line(long long a, long long b): a(a), b(b) {}
-    long long operator()(int x) const { return a * x + b; }
-  };
-  line st[maxc * 4];
-  int sz, lc[maxc * 4], rc[maxc * 4];
-  int gnode() {
-    st[sz] = line(1e9, 1e9);
-    lc[sz] = -1, rc[sz] = -1;
-    return sz++;
-  }
-  void init() {
-    sz = 0; 
-  }
-  void add(int l, int r, line tl, int o) {
-    bool lcp = st[o](l) > tl(l);
-    bool mcp = st[o]((l + r) / 2) > tl((l + r) / 2);
-    if (mcp) swap(st[o], tl);
-    if (r - l == 1) return;
-    if (lcp != mcp) {
-      if (lc[o] == -1) lc[o] = gnode();
-      add(l, (l + r) / 2, tl, lc[o]);
-    } else {
-      if (rc[o] == -1) rc[o] = gnode();
-      add((l + r) / 2, r, tl, rc[o]);
+// edu13F MLE with non-deleted pointers
+// [) interval for negative numbers
+constexpr i64 INF64 = 4e18;
+struct Line {
+    i64 a = -INF64, b = -INF64;
+    i64 operator()(i64 x) const {
+        if (a == -INF64 && b == -INF64) {
+            return -INF64;
+        } else {
+            return a * x + b;
+        }
     }
-  }
-  long long query(int l, int r, int x, int o) {
-    if (r - l == 1) return st[o](x);
-    if (x < (l + r) / 2) {
-      if (lc[o] == -1) return st[o](x);
-      return min(st[o](x), query(l, (l + r) / 2, x, lc[o]));
-    } else {
-      if (rc[o] == -1) return st[o](x);
-      return min(st[o](x), query((l + r) / 2, r, x, rc[o]));
+};
+constexpr int INF32 = 1e9;
+struct LiChao {
+    static constexpr int N = 5e6;
+    array<Line, N> st;
+    array<int, N> lc, rc;
+    int n = 0;
+    void clear() { n = 0; node(); }
+    int node() {
+        st[n] = {};
+        lc[n] = rc[n] = -1;
+        return n++;
     }
-  }}
+    void add(int id, int l, int r, Line line) {
+        int m = (l + r) / 2;
+        bool lcp = st[id](l) < line(l);
+        bool mcp = st[id](m) < line(m);
+        if (mcp) { swap(st[id], line); }
+        if (r - l == 1) { return; }
+        if (lcp != mcp) {
+            if (lc[id] == -1) {
+                lc[id] = node();
+            }
+            add(lc[id], l, m, line);
+        } else {
+            if (rc[id] == -1) {
+                rc[id] = node();
+            }
+            add(rc[id], m, r, line);
+        }
+    }
+    void add(Line line, int l = -INF32 - 1, int r = INF32 + 1) {
+        add(0, l, r, line);
+    }
+    i64 query(int id, int l, int r, i64 x) {
+        i64 res = st[id](x);
+        if (r - l == 1) { return res; }
+        int m = (l + r) / 2;
+        if (x < m && lc[id] != -1) {
+            res = max(res, query(lc[id], l, m, x));
+        } else if (x >= m && rc[id] != -1) {
+            res = max(res, query(rc[id], m, r, x));
+        }
+        return res;
+    }
+    i64 query(i64 x, int l = -INF32 - 1, int r = INF32 + 1) {
+        return query(0, l, r, x);
+    }
+};
