f88d1572043a594b2e725c9720d3849b85c720a3
b08901135@ntu.edu.tw <b08901135@ntu.edu.tw>
2023-10-21 02:39:25 +0800

    add convex hull operations from yhchang3

--- geometry/convexHull.cpp
+++ geometry/convexHull.cpp
@@ -1,152 +1,105 @@
-bool operator<(const P &a, const P &b) {
-  return same(a.x, b.x) ? a.y < b.y : a.x < b.x;
-}
-bool operator>(const P &a, const P &b) {
-  return same(a.x, b.x) ? a.y > b.y : a.x > b.x;
-}
+给定凸包, log n 内完成各种询问, 具体操作有 :
+1. 判定一个点是否在凸包内
+2. 询问凸包外的点到凸包的两个切点
+3. 询问一个向量关于凸包的切点
+4. 询问一条直线和凸包的交点
+ INF 为坐标范围, 需要定义点类大于号
+ 改成实数只需修改 sign 函数，以及把 long long 改为 double 即可
+ 构造函数时传入凸包要求无重点, 面积非空, 以及 pair(x,y) 的最小点放在第一个
 
-#define crx(a, b, c) ((b - a) ^ (c - a))
-
-vector<P> convex(vector<P> ps) {
-  vector<P> p;
-  sort(ps.begin(), ps.end(), [&] (P a, P b) { return same(a.x, b.x) ? a.y < b.y : a.x < b.x; });
-  for (int i = 0; i < ps.size(); ++i) {
-    while (p.size() >= 2 && crx(p[p.size() - 2], ps[i], p[p.size() - 1]) >= 0) p.pop_back();
-    p.push_back(ps[i]);
-  }
-  int t = p.size();
-  for (int i = (int)ps.size() - 2; i >= 0; --i) {
-    while (p.size() > t && crx(p[p.size() - 2], ps[i], p[p.size() - 1]) >= 0) p.pop_back();
-    p.push_back(ps[i]);
-  }
-  p.pop_back();
-  return p;
-}
-
-int sgn(double x) { return same(x, 0) ? 0 : x > 0 ? 1 : -1; }
-
-P isLL(P p1, P p2, P q1, P q2) {
-  double a = crx(q1, q2, p1), b = -crx(q1, q2, p2);
-  return (p1 * b + p2 * a) / (a + b);
-}
-
-struct CH {
+const int INF = 1e9;
+struct Convex {
   int n;
-  vector<P> p, u, d;
-  CH() {}
-  CH(vector<P> ps) : p(ps) {
-    n = ps.size();
-    rotate(p.begin(), min_element(p.begin(), p.end()), p.end());
-    auto t = max_element(p.begin(), p.end());
-    d = vector<P>(p.begin(), next(t));
-    u = vector<P>(t, p.end()); u.push_back(p[0]);
-  }
-  int find(vector<P> &v, P d) {
-    int l = 0, r = v.size();
-    while (l + 5 < r) {
-      int L = (l * 2 + r) / 3, R = (l + r * 2) / 3;
-      if (v[L] * d > v[R] * d) r = R;
-      else l = L;
+  vector<Point> a, upper, lower;
+  Convex(vector<Point> a_) : a(a_) {
+    n = a_.size();
+    int ptr = 0;
+    for (int i = 1; i < n; i++) {
+      if (a[ptr] < a[i]) ptr = i;
     }
-    int x = l;
-    for (int i = l + 1; i < r; ++i) if (v[i] * d > v[x] * d) x = i;
-    return x;
-  }
-  int findFarest(P v) {
-    if (v.y > 0 || v.y == 0 && v.x > 0) return ((int)d.size() - 1 + find(u, v)) % p.size();
-    return find(d, v);
-  }
-  P get(int l, int r, P a, P b) {
-    int s = sgn(crx(a, b, p[l % n]));
-    while (l + 1 < r) {
-      int m = (l + r) >> 1;
-      if (sgn(crx(a, b, p[m % n])) == s) l = m;
-      else r = m;
+    for (int i = 0; i <= ptr; i++) {
+      lower.push_back(a[i]);
     }
-    return isLL(a, b, p[l % n], p[(l + 1) % n]);
-  }
-  vector<P> getLineIntersect(P a, P b) {
-    int X = findFarest((b - a).rot(pi / 2));
-    int Y = findFarest((a - b).rot(pi / 2));
-    if (X > Y) swap(X, Y);
-    if (sgn(crx(a, b, p[X])) * sgn(crx(a, b, p[Y])) < 0) return {get(X, Y, a, b), get(Y, X + n, a, b)};
-    return {}; // tangent case falls here
-  }
-  void update_tangent(P q, int i, int &a, int &b) {
-    if (sgn(crx(q, p[a], p[i])) > 0) a = i;
-    if (sgn(crx(q, p[b], p[i])) < 0) b = i;
-  }
-  void bs(int l, int r, P q, int &a, int &b) {
-    if (l == r) return;
-    update_tangent(q, l % n, a, b);
-    int s = sgn(crx(q, p[l % n], p[(l + 1) % n]));
-    while (l + 1 < r) {
-      int m = (l + r) >> 1;
-      if (sgn(crx(q, p[m % n], p[(m + 1) % n])) == s) l = m;
-      else r = m;
+    for (int i = ptr; i < n; i++) {
+      upper.push_back(a[i]);
     }
-    update_tangent(q, r % n, a, b);
+    upper.push_back(a[0]);
   }
-  int x = l;
-  for (int i = l + 1; i < r; ++i) if (v[i] * d > v[x] * d) x = i;
-  return x;
-}
-int findFarest(P v) {
-  if (v.y > 0 || v.y == 0 && v.x > 0) return ((int)d.size() - 1 + find(u, v)) % p.size();
-  return find(d, v);
-}
-P get(int l, int r, P a, P b) {
-  int s = sgn(crx(a, b, p[l % n]));
-  while (l + 1 < r) {
-    int m = (l + r) >> 1;
-    if (sgn(crx(a, b, p[m % n])) == s) l = m;
-    else r = m;
+  int sign(long long x) { return x < 0 ? -1 : x > 0; }
+  pair<long long, int> get_tangent(vector<Point> &convex, Point vec) {
+    int l = 0, r = int(convex.size()) - 2;
+    for ( ; l + 1 < r; ) {
+      int mid = (l + r) / 2;
+      if (sign((convex[mid + 1] - convex[mid]).det(vec)) > 0) r = mid;
+      else l = mid;
+    }
+    return max(make_pair(vec.det(convex[r]), r), make_pair(vec.det(convex[0]), 0));
   }
-  return isLL(a, b, p[l % n], p[(l + 1) % n]);
-}
-vector<P> getIS(P a, P b) {
-  int X = findFarest((b - a).spin(pi / 2));
-  int Y = findFarest((a - b).spin(pi / 2));
-  if (X > Y) swap(X, Y);
-  if (sgn(crx(a, b, p[X])) * sgn(crx(a, b, p[Y])) < 0) return {get(X, Y, a, b), get(Y, X + n, a, b)};
-  return {};
-}
-void update_tangent(P q, int i, int &a, int &b) {
-  if (sgn(crx(q, p[a], p[i])) > 0) a = i;
-  if (sgn(crx(q, p[b], p[i])) < 0) b = i;
-}
-void bs(int l, int r, P q, int &a, int &b) {
-  if (l == r) return;
-  update_tangent(q, l % n, a, b);
-  int s = sgn(crx(q, p[l % n], p[(l + 1) % n]));
-  while (l + 1 < r) {
-    int m = (l + r) >> 1;
-    if (sgn(crx(q, p[m % n], p[(m + 1) % n])) == s) l = m;
-    else r = m;
+  void update_tangent(const Point &p, int id, int &i0, int &i1) {
+    if ((a[i0] - p).det(a[id] - p) > 0) i0 = id;
+    if ((a[i1] - p).det(a[id] - p) < 0) i1 = id;
   }
-  update_tangent(q, r % n, a, b);
-}
-bool contain(P p) {
-  if (p.x < d[0].x || p.x > d.back().x) return 0;
-  auto it = lower_bound(d.begin(), d.end(), P(p.x, -1e12));
-  if (it->x == p.x) {
-    if (it->y > p.y) return 0;
-  } else if (crx(*prev(it), *it, p) < -eps) return 0;
-  it = lower_bound(u.begin(), u.end(), P(p.x, 1e12), greater<P>());
-  if (it->x == p.x) {
-    if (it->y < p.y) return 0;
-  } else if (crx(*prev(it), *it, p) < -eps) return 0;
-  return 1;
-}
-bool get_tangent(P p, int &a, int &b) { // b -> a
-  if (contain(p)) return 0;
-  a = b = 0;
-  int i = lower_bound(d.begin(), d.end(), p) - d.begin();
-  bs(0, i, p, a, b);
-  bs(i, d.size(), p, a, b);
-  i = lower_bound(u.begin(), u.end(), p, greater<P>()) - u.begin();
-  bs((int)d.size() - 1, (int)d.size() - 1 + i, p, a, b);
-  bs((int)d.size() - 1 + i, (int)d.size() - 1 + u.size(), p, a, b);
-  return 1;
-}
-};
+  void binary_search(int l, int r, Point p, int &i0, int &i1) {
+    if (l == r) { return; }
+    update_tangent(p, l % n, i0, i1);
+    int sl = sign((a[l % n] - p).det(a[(l + 1) % n] - p));
+    for ( ; l + 1 < r; ) {
+      int mid = (l + r) / 2;
+      int smid = sign((a[mid % n] - p).det(a[(mid + 1) % n] - p));
+      if (smid == sl) l = mid;
+      else r = mid;
+    }
+    update_tangent(p, r % n, i0, i1);
+  }
+  int binary_search(Point u, Point v, int l, int r) {
+    int sl = sign((v - u).det(a[l % n] - u));
+    for( ; l + 1 < r; ) {
+      int mid=(l+r)/2;
+      int smid = sign((v - u).det(a[mid % n] - u));
+      if(smid==sl)l=mid;
+      else r = mid;
+    }
+    return l % n;
+  }
+  // 判定点是否在凸包内，在边界返回 true
+  bool contain(Point p) {
+    if (p.x < lower[0].x || p.x > lower.back().x) return false;
+    int id = lower_bound(lower.begin(), lower.end(), Point(p.x, -INF)) - lower.begin();
+    if (lower[id].x == p.x) {
+      if (lower[id].y > p.y) return false;
+    } else if ((lower[id - 1] - p).det(lower[id] - p) < 0) return false;
+    id = lower_bound(upper.begin(), upper.end(), Point(p.x, INF), greater<Point>()) - upper.begin();
+    if (upper[id].x == p.x) {
+      if (upper[id].y < p.y) return false;
+    } else if ((upper[id - 1] - p).det(upper[id] - p) < 0) return false;
+    return true;
+  }
+  // 求点 p 关于凸包的两个切点，如果在凸包外则有序返回编号，共线的多个切点返回任意一个，否则返回 false
+  bool get_tangent(Point p, int &i0, int &i1) {
+    if (contain(p)) return false;
+    i0 = i1 = 0;
+    int id = lower_bound(lower.begin(),lower.end(),p) - lower.begin();
+    binary_search(0, id, p, i0, i1);
+    binary_search(id, (int)lower.size(), p, i0, i1);
+    id = lower_bound(upper.begin(), upper.end(), p, greater<Point>()) - upper.begin();
+    binary_search((int)lower.size() - 1, (int)lower.size() - 1 + id, p, i0, i1);
+    binary_search((int)lower.size() - 1 + id, (int)lower.size() - 1 + (int)upper.size(), p, i0, i1);
+    return true;
+  }
+  // 求凸包上和向量 vec 叉积最大的点，返回编号，共线的多个切点返回任意一个
+  int get_tangent(Point vec) {
+    pair<long long, int> ret = get_tangent(upper, vec);
+    ret.second = (ret.second + int(lower.size()) - 1) % n;
+    ret = max(ret, get_tangent(lower, vec));
+    return ret.second;
+  }
+  // 求凸包和直线 u,v 的交点, 如果无严格相交返回 false. 如果有则是和(i,next(i)) 的交点, 两个点无序, 交在点上不确定返回前后两条线段其中之一
+  bool get_intersection(Point p, Point v, int &i0, int &i1) {
+    int p0 = get_tangent(u - v), p1 = get_tangent(v - u);
+    if (sign((v - u).det(a[p0] - u)) * sign((v - u).det(a[p1] - u)) < 0) {
+      if (p0 > p1) swap(p0, p1);
+      i0 = binary_search(u, v, p0, p1);
+      i1 = binary_search(u, v, p1, p0 + n);
+      return true;
+    } else { return false; }
+  }
